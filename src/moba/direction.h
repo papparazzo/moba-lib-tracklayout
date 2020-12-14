6499136d54be485ceefd425478b3f2f4b932d587
papparazzo <pappi-@gmx.de>
2020-12-14 20:16:05 +0100

    direction refactored

--- src/moba/direction.h
+++ src/moba/direction.h
@@ -22,163 +22,196 @@
 
 #include <string>
 #include <cstdint>
+#include <stdexcept>
+#include <ostream>
 
-enum Direction {
-    UNSET        = 0,
-    TOP          = 1,
-    TOP_RIGHT    = 2,
-    RIGHT        = 4,
-    BOTTOM_RIGHT = 8,
+struct Direction {
+    enum Position {
+        UNSET        = 0,
+        TOP          = 1,
+        TOP_RIGHT    = 2,
+        RIGHT        = 4,
+        BOTTOM_RIGHT = 8,
 
-    BOTTOM       = 16,
-    BOTTOM_LEFT  = 32,
-    LEFT         = 64,
-    TOP_LEFT     = 128
-};
+        BOTTOM       = 16,
+        BOTTOM_LEFT  = 32,
+        LEFT         = 64,
+        TOP_LEFT     = 128
+    };
 
-inline Direction getDirectionByArrowKeys(int key) {
-    switch(key) {
-        case 1:
-            return Direction::BOTTOM_LEFT;
-        case 2:
-            return Direction::BOTTOM;
-        case 3:
-            return Direction::BOTTOM_RIGHT;
-        case 4:
-            return Direction::LEFT;
-        case 5:
-            return Direction::UNSET;
-        case 6:
-            return Direction::RIGHT;
-        case 7:
-            return Direction::TOP_LEFT;
-        case 8:
-            return Direction::TOP;
-        case 9:
-            return Direction::TOP_RIGHT;
+    Direction() : position{UNSET} {
     }
-}
 
-inline std::string getDirectionAsString(Direction dir) {
-    switch(dir) {
-        case Direction::UNSET:
-            return "UNSET";
-
-        case Direction::TOP:
-            return "TOP";
-
-        case Direction::TOP_RIGHT:
-            return "TOP_RIGHT";
-
-        case Direction::RIGHT:
-            return "RIGHT";
-
-        case Direction::BOTTOM_RIGHT:
-            return "BOTTOM_RIGHT";
-
-        case Direction::BOTTOM:
-            return "BOTTOM";
-
-        case Direction::BOTTOM_LEFT:
-            return "BOTTOM_LEFT";
-
-        case Direction::LEFT:
-            return "LEFT";
-
-        case Direction::TOP_LEFT:
-            return "TOP_LEFT";
+    Direction(Position position) : position{position} {
     }
-}
 
-inline Direction getNextLeftDirection(Direction dir) {
-    if(dir == TOP) {
-        return Direction::TOP_LEFT;
+    Direction(std::uint8_t position) {
+        switch(position) {
+            case UNSET:
+            case TOP:
+            case TOP_RIGHT:
+            case RIGHT:
+            case BOTTOM_RIGHT:
+
+            case BOTTOM:
+            case BOTTOM_LEFT:
+            case LEFT:
+            case TOP_LEFT:
+                this->position = position;
+                break;
+
+            default:
+                throw std::out_of_range{"invalid position given"};
+        }
     }
-    return static_cast<Direction>(dir / 2);
-}
 
-inline Direction getNextRightDirection(Direction dir) {
-    if(dir == Direction::TOP_LEFT) {
-        return TOP;
+    static Direction getDirectionByArrowKeys(int key) {
+        switch(key) {
+            case 1:
+                return Direction::BOTTOM_LEFT;
+            case 2:
+                return Direction::BOTTOM;
+            case 3:
+                return Direction::BOTTOM_RIGHT;
+            case 4:
+                return Direction::LEFT;
+            case 5:
+                return Direction::UNSET;
+            case 6:
+                return Direction::RIGHT;
+            case 7:
+                return Direction::TOP_LEFT;
+            case 8:
+                return Direction::TOP;
+            case 9:
+                return Direction::TOP_RIGHT;
+        }
     }
-    return static_cast<Direction>(dir * 2);
-}
 
-inline Direction rotateLeft(Direction dir, std::uint8_t steps = 1) {
-    steps %= 8;
-    for(std::uint8_t i = 0; i < steps; ++i ) {
-        dir = getNextLeftDirection(dir);
+    operator int() const {
+        return position;
     }
-    return dir;
-}
 
-inline Direction rotateRight(Direction dir, std::uint8_t steps = 1) {
-    steps %= 8;
-    for(std::uint8_t i = 0; i < steps; ++i ) {
-        dir = getNextRightDirection(dir);
+    Direction& operator++() {
+        position = (position << 1) | (position >> 7);
+        return *this;
     }
-    return dir;
-}
 
-/**
- * Gibt die Entgegengesetzt Richtung zurück. Beispiel: TOP liefert BOTTOM
- *
- * @param dir
- * @return
- */
-inline Direction getComplementaryDirection(Direction dir) {
-    if(dir == Direction::UNSET) {
-        return Direction::UNSET;
+    Direction operator++(int) {
+        Direction tmp(*this);
+        operator++();
+        return tmp;
     }
-    if(dir < BOTTOM) {
-        return static_cast<Direction>(dir * 16);
+
+    Direction& operator+=(std::uint8_t steps) {
+        steps &= 7;
+        position = (position << steps) | (position >> (-steps & 7));
+        return *this;
     }
-    return static_cast<Direction>(dir / 16);
-}
 
-enum class DistanceType {
-    INVALID = 0,
-    STRAIGHT = 1,
-    LEFT_BEND = 2,
-    RIGHT_BEND = 3
-};
+    friend Direction operator+(Direction lhs, std::uint8_t steps) {
+        lhs += steps;
+        return lhs;
+    }
 
-/**
-* Die Distanz zwischen zwei Verbindungspunkte muss mindestens 3 Bit betragen, damit
-* zwei 2 Verbindungspunkte (auch als Teil einer Weiche) ein gültiges Gleis bilden.
-* Zu einem Verbindungspunkt "dirIn" kommen nur 3 mögliche Verbindungspunkte "dirOut" in Frage:
-* 1. Der komplemntäre Verbindungspunkt (also ein gerades Gleis)
-* 2. Der komplemntäre Verbindungspunkt + 1 Bit (also rechts gebogenes Gleis)
-* 3. Der komplemntäre Verbindungspunkt - 1 Bit (also links gebogenes Gleis)
-*
-* @param dir1
-* @param dir2
-* @return DistanceType
-*/
-inline DistanceType getDistanceType(Direction dirIn, Direction dirOut) {
-   if(dirIn == dirOut) {
+    Direction& operator--() {
+        position = (position >> 1) | (position << 7);
+        return *this;
+    }
+
+    Direction operator--(int) {
+        Direction tmp(*this);
+        operator++();
+        return tmp;
+    }
+
+    Direction& operator-=(std::uint8_t steps) {
+        steps &= 7;
+        position = (position >> steps) | (position << (-steps & 7));
+        return *this;
+    }
+
+    friend Direction operator-(Direction lhs, std::uint8_t steps) {
+        lhs -= steps;
+        return lhs;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Direction& dir) {
+        switch(dir.position) {
+            case Direction::UNSET:
+                return os << "UNSET";
+
+            case Direction::TOP:
+                return os << "TOP";
+
+            case Direction::TOP_RIGHT:
+                return os << "TOP_RIGHT";
+
+            case Direction::RIGHT:
+                return os << "RIGHT";
+
+            case Direction::BOTTOM_RIGHT:
+                return os << "BOTTOM_RIGHT";
+
+            case Direction::BOTTOM:
+                return os << "BOTTOM";
+
+            case Direction::BOTTOM_LEFT:
+                return os << "BOTTOM_LEFT";
+
+            case Direction::LEFT:
+                return os << "LEFT";
+
+            case Direction::TOP_LEFT:
+                return os << "TOP_LEFT";
+
+            default:
+                throw std::out_of_range{"invalid position given"};
+        }
+    }
+
+    enum DistanceType {
+        INVALID = 0,
+        STRAIGHT = 1,
+        LEFT_BEND = 2,
+        RIGHT_BEND = 3
+    };
+
+    /**
+    * Die Distanz zwischen zwei Verbindungspunkte muss mindestens 3 Bit betragen, damit
+    * zwei 2 Verbindungspunkte (auch als Teil einer Weiche) ein gültiges Gleis bilden.
+    * Zu einem Verbindungspunkt "dirIn" kommen nur 3 mögliche Verbindungspunkte "dirOut" in Frage:
+    * 1. Der komplemntäre Verbindungspunkt (also ein gerades Gleis)
+    * 2. Der komplemntäre Verbindungspunkt + 1 Bit (also rechts gebogenes Gleis)
+    * 3. Der komplemntäre Verbindungspunkt - 1 Bit (also links gebogenes Gleis)
+    *
+    * @param dir1
+    * @param dir2
+    * @return DistanceType
+    */
+    inline DistanceType getDistanceType(Direction dirOut) {
+       auto dirInCom = getComplementaryDirection();
+
+       if(dirOut == ++dirInCom) {
+           return DistanceType::RIGHT_BEND;
+       }
+
+       if(dirOut == --dirInCom) {
+           return DistanceType::STRAIGHT;
+       }
+
+       if(dirOut == --dirInCom) {
+           return DistanceType::LEFT_BEND;
+       }
+
        return DistanceType::INVALID;
-   }
+    }
 
-   auto dirInCom = getComplementaryDirection(dirIn);
+    Direction getComplementaryDirection() const {
+        return (position << 4) | (position >> 4);
+    }
 
-   if(dirOut == dirInCom) {
-       return DistanceType::STRAIGHT;
-   }
+protected:
+    std::uint8_t position;
 
-   if(dirOut == (dirInCom * 2)) {
-       return DistanceType::RIGHT_BEND;
-   }
-
-   // Sonderfall: TOP == 1 -> 1 / 2 = 0 -> müsste hier jedoch 128 sein!!
-   if(dirInCom == TOP && dirOut == TOP_LEFT) {
-       return DistanceType::LEFT_BEND;
-   }
-
-   if(dirOut == (dirInCom / 2)) {
-       return DistanceType::LEFT_BEND;
-   }
-
-   return DistanceType::INVALID;
-}
-
+};
